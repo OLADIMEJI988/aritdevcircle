ff8553c93379e3ad2ff04df83c1a548646a5d578
OLADIMEJI988 <oladimejimax49@gmail.com>
2025-08-21 19:09:04 +0100

    changed hero

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,11 +6,18 @@ import glow from "../assets/inner glow.png";
 import arrow from "../assets/arrow-right.png";
 import leftimg from "../assets/Hero circular image left.png";
 import rightimg from "../assets/Hero circlar image right.png";
+import topimg from "../assets/Hero circlar image horizontalTop.png";
+import bottomimg from "../assets/Hero circlar image horizontalBottom.png";
+import mobiletopimg from "../assets/Hero circlar image Topmobile.png";
+import mobilebottomimg from "../assets/Hero circlar image Bottommobile.png";
+
+
+
 
 export default function LandingPage() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const heading = "Transform Your Tech Skills Into A Career That Pays.".split(
+  const heading = "From Learning To Earning".split(
     " "
   );
 
@@ -18,28 +25,28 @@ export default function LandingPage() {
     <div className="relative w-full min-h-screen bg-[#0E0E0E]">
       {/* Glow Background*/}
       <div
-        className="absolute inset-0 w-full h-full bg-no-repeat bg-center bg-cover pointer-events-none"
+        className="absolute inset-0 w-full lg:max-xl:h-[800px] xl:h-[920px] bg-no-repeat bg-center bg-cover pointer-events-none"
         style={{ backgroundImage: `url(${glow})` }}
       />
 
       {/* Header */}
-      <header className="sticky md:static top-0 bg-[#0e1319] py-1 md:py-0 md:bg-[#0E0E0E] text-[#BBBABA] px-5 md:px-9 z-20">
+      <header className="sticky lg:static top-0 bg-[#0e1319] py-1 md:py-0 lg:bg-[#0E0E0E] text-[#BBBABA] px-5 md:px-[15px] z-20">
         <div className="flex justify-between items-center">
-          <img className="h-12 md:h-16" src={Logo} alt="logo" />
+          <img className="h-16" src={Logo} alt="logo" />
 
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="md:hidden text-2xl focus:outline-none"
+            className="lg:hidden text-2xl focus:outline-none"
           >
             {isOpen ? (
-              <i className="fa-solid fa-x fa-xs"></i>
+              <i className="fa-solid fa-x"></i>
             ) : (
-              <i className="fa-solid fa-bars fa-xs"></i>
+              <i className="fa-solid fa-bars "></i>
             )}
           </button>
 
           {/* Desktop Menu */}
-          <div className="hidden md:flex gap-7 xl:gap-10 text-sm md:text-[15px] xl:text-[19px] items-center">
+          <div className="hidden lg:flex gap-7 xl:gap-10 text-sm lg:text-[15px] xl:text-[19px] items-center">
             {["About", "Mentorship", "Testimonials", "Programmes", "FAQ"].map(
               (item, idx) => (
                 <p
@@ -51,7 +58,7 @@ export default function LandingPage() {
                 </p>
               )
             )}
-            <button className="bg-[#fc4f7b] text-white xl:text-[19px] flex justify-center py-2 xl:pt-3 xl:pb-4 px-7 rounded-full font-semibold hover:opacity-90 transition">
+            <button className="bg-[#fc4f7b] text-white xl:text-[20px] flex justify-center py-2 xl:pt-3 xl:pb-4 px-7 rounded-full font-semibold hover:opacity-90 transition">
               Join it’s Free
             </button>
           </div>
@@ -66,15 +73,15 @@ export default function LandingPage() {
               animate={{ height: "auto", opacity: 1 }}
               exit={{ height: 0, opacity: 0 }}
               transition={{ duration: 0.3, ease: "easeInOut" }}
-              className="absolute top-full left-0 w-full bg-[#0e1319] md:hidden z-20 overflow-hidden"
+              className="absolute top-full left-0 w-full bg-[#0e1319] lg:hidden z-20 overflow-hidden"
             >
-              <div className="flex flex-col gap-5 text-[16px] text-center py-8">
+              <div className="flex flex-col gap-5 mobile:max-lg:text-[25px] text-center py-8">
                 <p className="hover:cursor-pointer">About</p>
                 <p className="hover:cursor-pointer">Mentorship</p>
                 <p className="hover:cursor-pointer">Testimonials</p>
                 <p className="hover:cursor-pointer">Programmes</p>
                 <p className="hover:cursor-pointer">FAQ</p>
-                <button className="bg-[#fc4f7b] flex items-center text-white py-2 px-[29px] rounded-full font-semibold hover:opacity-90 transition max-w-48 mx-auto">
+                <button className="bg-[#fc4f7b] flex items-center text-white py-3 px-[32px] rounded-full font-semibold hover:opacity-90 transition max-w-60 mx-auto">
                   Join it’s Free
                 </button>
               </div>
@@ -83,7 +90,7 @@ export default function LandingPage() {
         </AnimatePresence>
       </header>
 
-      <div className="flex flex-col items-center md:flex-row mx-9 justify-between mt-8 relative z-10">
+      <div className="flex flex-col mobile:max-md:gap-9 gap-7 items-center lg:flex-row mx-[15px] justify-between mobile:max-md:mt-24 md:max-lg:mt-[3vh] lg:mt-[11vh] relative z-10">
         {/* Left Image */}
         <motion.div
             className="mr-0 md:mr-4 -mt-[70px] md:mt-0 order-first md:order-none"
@@ -91,28 +98,30 @@ export default function LandingPage() {
             animate={{ opacity: 1, x: 0 }}
             transition={{ delay: 1.5, duration: 0.4, ease: "easeOut" }}
         >
-            <img className="h-[370px] md:h-[510px] xl:h-full rotate-90 md:rotate-0" src={leftimg} alt="left hero" />
+            <img className="h-auto mobile:max-lg:hidden lg:h-[510px] xl:h-full md:w-full rotate-90 lg:rotate-0" src={leftimg} alt="left hero" />
+            <img className="h-auto mobile:max-md:hidden md:max-lg:max-h-full lg:hidden md:w-full lg:rotate-0" src={topimg} alt="left hero" />
+            <img className="h-auto moblie:max-md:max-h-full md:hidden md:w-full lg:rotate-0" src={mobiletopimg} alt="left hero" />
         </motion.div>
 
         {/* Center Content */}
-        <div className="-mt-20 md:mt-0 text-center order-2 md:order-none">
+        <div className="mobile:max-md:mt-2 md:max-lg:-mt-4 lg:-mt-24 text-center order-2 md:order-none">
             <motion.div
                 initial={{ opacity: 0, y: -10 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.4 }}
                 className="p-[1px] border border-[#fc4f7b] rounded-full w-fit mx-auto mt-1 md:mt-10"
                 >
-                <div className="rounded-full py-1 px-3 flex justify-center items-center gap-1">
-                    <img src={flash} alt="lightening img" className="h-[16px] md:h-[18px]" />
-                    <p className="text-[#D9D9D9] text-[13px] xl:text-[16px]">
-                    From Learning to Earning
+                <div className="rounded-full py-[11px] mobile:max-lg:py-4 lg:py-[8px] xl:py-[11px] px-4 flex justify-center items-center gap-3 md:gap-2">
+                    <img src={flash} alt="lightening img" className="mobile:max-lg:h-[23px] lg:h-[18px] xl:h-[22px]" />
+                    <p className="text-[#D9D9D9] mobile:max-lg:text-[18px] lg:text-[14px] xl:text-[19px]">
+                    Tech Community Empowerment
                     </p>
                 </div>
             </motion.div>
 
-            <div className="max-w-[95%] md:max-w-[680px] xl:max-w-[820px] mx-auto mt-5 md:mt-4 xl:mt-6">
+            <div className="lg:max-w-[680px] xl:max-w-full mx-auto mt-8 lg:mt-4 xl:mt-6">
             <motion.p
-                className="text-[#E3E3E3] text-[35px] sm:text-4xl md:text-5xl xl:text-6xl font-semibold leading-snug md:leading-[60px] tracking-normal flex flex-wrap justify-center"
+                className="text-[#E3E3E3] mobile:max-lg:text-7xl lg:text-[55px] xl:text-[66px] font-semibold leading-snug md:leading-[60px] tracking-normal flex flex-wrap justify-center"
                 initial="hidden"
                 animate="visible"
                 variants={{
@@ -136,40 +145,49 @@ export default function LandingPage() {
             </motion.p>
 
             <motion.p
-                className="text-[#BBBABA] text-[15px] md:text-[13px] xl:text-[16px] max-w-[550px] mx-auto mt-4 xl:mt-6"
+                className="text-[#BBBABA] mobile:max-lg:text-[24px] lg:text-[15px] xl:text-[18px] lg:max-w-[550px] mx-auto mobile:max-lg:mt-10 lg:mt-[22px] xl:mt-8"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.4, delay: 0.9 }}
             >
-                An Authentic community empowering early career tech professionals to thrive through mentorship, real world challenges and opportunities.
+                Transform your tech skills into a career that pays.
             </motion.p>
             </div>
 
             {/* CTA Button */}
             <motion.button
-            className="bg-[#fc4f7b] text-[#110D0D] py-2 px-5 rounded-full font-semibold hover:opacity-90 flex justify-center items-center gap-[14px] md:gap-3 mx-auto mt-6 md:mt-5 xl:mt-7"
+            className="bg-[#fc4f7b] text-[#110D0D] mobile:max-lg:py-5 lg:py-2 xl:py-3 mobile:max-lg:px-9 lg:px-5 xl:px-6 rounded-full font-semibold hover:opacity-90 flex justify-center items-center mobile:max-lg:gap-4 lg:gap-3 mx-auto mobile:max-lg:mt-8 lg:mt-6 xl:mt-8"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.4, delay: 1.3, ease: "easeOut" }}
             >
-            <p className="text-[16px] xl:text-[18px] text-white">Join the Circle Now</p>
-            <div className="mt-[2px] bg-white p-[5px] rounded-full">
-                <img className="h-5" src={arrow} alt="arrow" />
+            <p className="mobile:max-lg:text-[25px] lg:text-[16px] xl:text-[19px] text-white">Join the Circle Now</p>
+            <div className="mt-[2px] bg-white p-[5px] xl:p-[6px] rounded-full">
+                <img className="mobile:max-lg:h-7 xl:h-6" src={arrow} alt="arrow" />
             </div>
             </motion.button>
         </div>
 
         {/* Right Image */}
         <motion.div
-            className="ml-0 -mt-16 md:mt-0 md:ml-4 flex justify-center items-center order-last md:order-none"
+            className="ml-0 mobile:max-lg:mt-4 lg:mt-0 md:ml-4 flex justify-center items-center order-last md:order-none"
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ delay: 1.6, duration: 0.4, ease: "easeOut" }}
         >
-            <img className="h-[370px] md:h-[510px] xl:h-full rotate-90 md:rotate-0" src={rightimg} alt="right hero" />
+            <img className="h-auto mobile:max-lg:hidden lg:h-[510px] xl:h-full md:w-full rotate-90 lg:rotate-0" src={rightimg} alt="right hero" />
+            <img className="h-auto mobile:max-md:hidden md:max-lg:max-h-full lg:hidden md:w-full lg:rotate-0" src={bottomimg} alt="left hero" />
+            <img className="h-auto moblie:max-md:max-h-full md:hidden md:w-full lg:rotate-0" src={mobilebottomimg} alt="left hero" />
+
+
         </motion.div>
+      </div>
+      <div className="h-48 ">
+
       </div>
 
     </div>
   );
 }
+
+
