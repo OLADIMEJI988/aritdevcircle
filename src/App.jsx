b21f69694fb2c225b3c612ce518642bae0f20786
OLADIMEJI988 <oladimejimax49@gmail.com>
2025-09-08 19:38:57 +0100

    hackathonremake

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import './index.css' 
 import LandingPage from './pages/LandingPage';
-import HackathonPage from "./pages/HackathonPage";
 import ScrollToTop from "./components/ScrollToTop";
 import HackathonPageRework from "./pages/HackathonPageRework";
 
